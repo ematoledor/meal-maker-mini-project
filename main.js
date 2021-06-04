ffac249a4ffc54c31c5604838199959ba07c239b
Emanuel Toledo <ematolr@gmail.com>
2021-06-03 19:02:00 -0500

    getters and setters.

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 // Mini Javascript project from codecademy done in terminal. (About Objects and methods.)
+
+let menu = {
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  },
+  get appetizers() {
+   
+  },
+  set appetizers(appetizerIn) {
+
+  },
+  get mains() {
+    
+  },
+  set mains(mainsIn) {
+ 
+  },
+  get desserts() {
+  
+  },
+  set desserts(dessertsIn) {
+ 
+ },
+};
