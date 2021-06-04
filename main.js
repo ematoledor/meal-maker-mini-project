664281995c4d37af9e63ab713916d2540a957d16
Emanuel Toledo <ematolr@gmail.com>
2021-06-03 23:15:27 -0500

    Finished mini project

--- main.js
+++ main.js
@@ -38,15 +38,36 @@ const menu = {
       price: dishPrice  
     };
     this._courses[courseName].push(dish);
-  }
+  },
   getRandomDishFromCourse(courseName) {
     const dishes = this._courses[courseName];
     const randomIndex = Math.floor(Math.random() * dishes.length);
     return dishes[randomIndex];
   },
   generateRandomMeal() {
-    const appetizers = this.getRandomDishFromCourse(['appetizers']);
-    const mains = this.getRandomDishFromCourse(['mains']);
-    const desserts = this.getRandomDishFromCourse(['desserts']);
+    const appetizer = this.getRandomDishFromCourse(['appetizers']);
+    const main = this.getRandomDishFromCourse(['mains']);
+    const dessert = this.getRandomDishFromCourse(['desserts']);
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name}. The price is ${totalPrice}`;
   }
 };
+
+menu.addDishToCourse('appetizers', 'hummus', 4.00);
+menu.addDishToCourse('appetizers', 'guac', 4.00);
+menu.addDishToCourse('appetizers', 'hidden', 8.00);
+
+menu.addDishToCourse('mains', 'meat', 15.00);
+menu.addDishToCourse('mains', 'fish', 14.00);
+menu.addDishToCourse('mains', 'other', 10.00);
+
+menu.addDishToCourse('desserts', 'ice cream', 5.00);
+menu.addDishToCourse('desserts', 'chocolate', 2.00);
+menu.addDishToCourse('desserts', 'arroz con leche', 7.00);
+
+const meal = menu.generateRandomMeal();
+const meal2 = menu.generateRandomMeal();
+const meal3 = menu.generateRandomMeal();
+console.log(meal);
+console.log(meal2);
+console.log(meal3);
